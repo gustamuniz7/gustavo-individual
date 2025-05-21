7d1d67861acbb6cfc0e6c123143d9d68eb71f086
muniz <gustavo.doliveira@sptech.school>
2025-05-20 22:06:23 -0300

    modelagem v2

--- Banco de dados/script-spfc.sql
+++ Banco de dados/script-spfc.sql
@@ -2,36 +2,20 @@ create database if not exists saopaulo;
 use saopaulo;
 
 create table usuario(
-idUsuario int primary key auto_increment,
+id int primary key auto_increment,
 nome varchar(100),
 email varchar(60),
 senha varchar(10)
 );
 
-create table pergunta(
-id int primary key auto_increment,
-pergunta varchar(100)
-);
 
-create table resposta(
+create table quiz(
 id int primary key auto_increment,
-fkpergunta int,
-respostas varchar(100),
 corretas int,
-foreign key (fkpergunta) references pergunta(id)
-);
-
-create table resposta_usuario(
-id int primary key auto_increment,
 fkusuario int,
-fkpergunta int,
-fkresposta int,
-acertou int,
-foreign key (fkusuario) references usuario(idUsuario),
-foreign key (fkpergunta) references pergunta(id),
-foreign key(fkresposta) references resposta(id)
+foreign key (fkusuario) references usuario(id)
 );
 
-drop database saopaulo;
+select * from usuario;
 
-
+select * from quiz;
