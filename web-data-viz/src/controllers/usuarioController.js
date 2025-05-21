fe6c615104f073829fa121f7e23b491d669da113
muniz <gustavo.doliveira@sptech.school>
2025-05-21 12:26:05 -0300

    adicionando jogador favorito ao cadastro

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -24,6 +24,7 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
+                                        jogador: resultadoAutenticar[0].jogador
                                     });
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
@@ -47,7 +48,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-
+    var jogador = req.body.jogadorServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -56,10 +57,12 @@ function cadastrar(req, res) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
+    } else if (jogador == undefined) {
+        res.status(400).send("Seu jogador está undefined!");
     }
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, senha, jogador)
             .then(
                 function (resultado) {
                     res.json(resultado);
