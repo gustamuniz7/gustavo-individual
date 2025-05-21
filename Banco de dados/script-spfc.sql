5bca3e585114589af34ce34339a63df443b98744
muniz <gustavo.doliveira@sptech.school>
2025-05-21 12:53:19 -0300

    banco de dados v2

--- Banco de dados/script-spfc.sql
+++ Banco de dados/script-spfc.sql
@@ -1,16 +1,17 @@
 create database if not exists saopaulo;
 use saopaulo;
 
-create table usuario(
+create table usuario (
 id int primary key auto_increment,
 nome varchar(100),
 email varchar(60),
-senha varchar(10)
+senha varchar(10),
+jogador varchar(50)
 );
 
 
 create table quiz(
-id int primary key auto_increment,
+idtTentativa int primary key auto_increment,
 corretas int,
 fkusuario int,
 foreign key (fkusuario) references usuario(id)
@@ -19,3 +20,6 @@ foreign key (fkusuario) references usuario(id)
 select * from usuario;
 
 select * from quiz;
+
+select sum(corretas) from quiz;
+
